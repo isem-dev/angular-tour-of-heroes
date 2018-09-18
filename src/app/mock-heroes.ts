4068fbfe6e07e29e0c2203e80c890373968ab75b
isemenov <igor_semenov_tp@bmc.com>
2018-09-18 10:31:28 +0300

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Konstantin Kramarenko' },
+  { id: 12, name: 'Mariia Chernenko' },
+  { id: 13, name: 'Aleksandr Makieiev' },
+  { id: 14, name: 'Anna Gromich' },
+  { id: 15, name: 'Taras Butkevych' },
+  { id: 16, name: 'Igor Semenov' },
+  { id: 17, name: 'Oleg Maslaw' },
+  { id: 18, name: 'Serhii Andriiash' },
+  { id: 19, name: 'Kateryna Demidova' },
+  { id: 20, name: 'Mr. Ramp' }
+];
